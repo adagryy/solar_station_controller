b7fd765c4af76164db6e9e486713c1070559fab8
Adam <adagryy@gmail.com>
2019-10-18 14:32:01 +0200

    Added 24-hour domain for x-axis into temperatures chart

--- mirrors/webapp/static/js/chartings.js
+++ mirrors/webapp/static/js/chartings.js
@@ -1,10 +1,11 @@
-async function drawChart(ctx) {
+async function drawChart(ctx, readingInterval) {
     // Get data from server for drawin on the chart
     let dataForChart = await fetch('https://' + window.location.host + '/viewer/drawChart/');
     let responseBody = await dataForChart.json();
 
     // console.log(responseBody);
-    let chartLabels = createXAsixLabels(responseBody);
+    let chartLabels = createXAsixLabels(responseBody, readingInterval);
+
     let chartData = generateDataForCharts(responseBody);
 
     var myChart = new Chart(ctx, {
@@ -37,6 +38,11 @@ async function drawChart(ctx) {
             }]
         },
         options: {
+            elements: {
+                line: {
+                    tension: 0 // disables bezier curves
+                }
+            },
             responsive: true,
             scales: {
                 xAxes: [{
@@ -58,10 +64,25 @@ async function drawChart(ctx) {
     });
 }
 
-function createXAsixLabels(array) {
-    return array.map((item) => {
-        return new Date(item.dateOfReading).toTimeString().slice(0, 5)
+function createXAsixLabels(array, readingInterval) {
+    let timeArr = array.map((item) => {
+        let date = new Date(item.dateOfReading);
+        return date.toLocaleDateString().slice(0, 10) + " " + date.toTimeString().slice(0, 5);
     });
+
+    let requiredNumberOfmeasurements = 288; // 24hours * 12 readings per hour.  ==> for readingInterval = 5 minutes that is:  288 = 24 * (60 / 5)
+    if (array.length >= requiredNumberOfmeasurements) { // If array has all required time labels, there is no need to create empty labels (dates, which has not a readings)
+        return timeArr;
+    }
+    let lackingElementsAmount = requiredNumberOfmeasurements - timeArr.length // We estimate how much readings are lacking
+    let lackingElements = [] // Array for storing lacking dates of readings
+    let startingDate = new Date(array[0].dateOfReading); // Create date from which we are starting countdown
+
+    for (i = 0; i < lackingElementsAmount; i++) {
+        let currentDate = new Date(startingDate.setMinutes(startingDate.getMinutes() - readingInterval)); // Get next date, which has not assigned any temperature reading
+        lackingElements.push(currentDate.toLocaleDateString().slice(0, 10) + " " + currentDate.toTimeString().slice(0, 5)); // ...and store it in array
+    }
+    return (lackingElements.reverse()).concat(timeArr); // Reverse order of array with lacking date labels and join it with labels, which already have a readings. 
 }
 
 function generateDataForCharts(array) {
@@ -70,10 +91,12 @@ function generateDataForCharts(array) {
         rightSensorReadings = [],
         tankSensorReadings = [];
     array.forEach((item) => {
-        leftSensorReadings.push(Math.round(item.leftSensorTemperature));
-        middleSensorReadings.push(item.middleSensorTemperature);
-        rightSensorReadings.push(item.rightSensorTemperature);
-        tankSensorReadings.push(item.tankSensorTemperature);
+        let readingDate = new Date(item.dateOfReading);
+        let formattedDate = readingDate.toLocaleDateString().slice(0, 10) + " " + readingDate.toTimeString().slice(0, 5);
+        leftSensorReadings.push({ x: formattedDate, y: item.leftSensorTemperature });
+        middleSensorReadings.push({ x: formattedDate, y: item.middleSensorTemperature });
+        rightSensorReadings.push({ x: formattedDate, y: item.rightSensorTemperature });
+        tankSensorReadings.push({ x: formattedDate, y: item.tankSensorTemperature });
     });
     return { leftSensorReadings, middleSensorReadings, rightSensorReadings, tankSensorReadings }
 }

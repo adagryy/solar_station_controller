b496dd1779771fd6e578f0fa7fd7f1efee1d67ac
Adam Gryczka <adagryy@gmail.com>
2019-11-08 00:07:59 +0100

    First approach to counting real sun rays declination

--- mirrors/webapp/static/js/chartings.js
+++ mirrors/webapp/static/js/chartings.js
@@ -1,6 +1,6 @@
 async function drawChart(ctx, xDomain) {
     // Get data from server for drawin on the chart
-    let dataForChart = await fetch('https://' + window.location.host + '/viewer/drawChart/');
+    let dataForChart = await fetch('http://' + window.location.host + '/viewer/drawChart/');
     let responseBody = await dataForChart.json();
 
     let resetRef = function() {
@@ -103,15 +103,26 @@ function generateDataForCharts(array) {
     return { leftSensorReadings, middleSensorReadings, rightSensorReadings, tankSensorReadings }
 }
 
-function dayInYear(element, element_label, latitude) {
-    let now = new Date();
+function dayInYear(element, element_label, latitude, longitude = 21.91) {
+    // let now = new Date();
+    let now = new Date("2019-11-08T10:00:16.267Z");
     let start = new Date(now.getFullYear(), 0, 0);
     let diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
     let oneDay = 1000 * 60 * 60 * 24;
     let N = Math.floor(diff / oneDay); // Day of year
     let rad = (degrees) => { return degrees * (Math.PI / 180); } // Converts degrees to radians
     let deg = (radians) => { return (180 * radians) / Math.PI; } // Converts radians to degrees
-    let rays_angle = 90 - latitude - deg(Math.asin((0.39779 * Math.cos(rad(0.98565 * (N + 10) + 1.914 * Math.sin(rad(0.98565 * (N - 2)))))))); // Formula for evaluating the angle between the rays of the Sun and the plane of the Earth's equator; https://en.wikipedia.org/wiki/Position_of_the_Sun#Calculations
+    let angle = -deg(Math.asin((0.39779 * Math.cos(rad(0.98565 * (N + 10) + 1.914 * Math.sin(rad(0.98565 * (N - 2)))))))); // Formula for evaluating the angle between the rays of the Sun and the plane of the Earth's equator; https://en.wikipedia.org/wiki/Position_of_the_Sun#Calculations
+    let noon_sun_declination = 90 - latitude + angle;
     element_label.innerHTML = `Kąt padania promieni słonecznych w południe (52&#176;N, ` + now.toLocaleDateString().slice(0, 10) + `)`;
-    element.innerHTML = (Math.round(rays_angle * 100) / 100).toString() + ` &#176;`;
+    element.innerHTML = (Math.round(noon_sun_declination * 100) / 100).toString() + ` &#176;`;
+     
+    let dateDifferenceInMinutes = Math.round((Math.abs(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds()) - 
+            Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 12, 0, now.getUTCSeconds()))) / 60000); // Calculates difference in minutes from current UTC time to UTC noon.
+    let currentZenithalSunLongitude = dateDifferenceInMinutes / 4; // Earth rotates 1 degree per 4 minutes.
+
+    let K1 = latitude - angle;   
+    let K2 = longitude - currentZenithalSunLongitude;
+    console.log("Różnica: " + dateDifferenceInMinutes);
+    console.log(K2)
 }
